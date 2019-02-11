b4ef783ef9abcada1e11988932d7cb299ee73e9c
张呈祥 <zhangchx@xmylsw.com>
2019-02-11 09:11:57 +0800

    新增pdf预览

---
+++ src/views/pdftest/XPdfDemo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!--<vuepdf src="./helloworld.pdf"></vuepdf>-->
+  </div>
+</template>
+
+<script>
+// import vuepdf from 'vue-pdf'
+export default {
+  name: 'pdf-demo',
+  data () {
+  },
+  methods: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
